2cd7447e0428e00bf8dea5fb182370d1e09911e6
Muhammad Zain Khan <9dmuhammadzainkhan@gmail.com>
2025-01-27 12:20:09 -0800

    lab1q4.cpp

---
+++ lab1q4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+ float weightkg;
+ float weightp;
+  
+cout <<"Enter weight in Kilograms: ";
+cin>>weightkg;
+weightp = weightkg * 2.2;
+cout<<"\nWeight in Kiograms: "<<weightkg;
+cout<<"\nWeight in Pounds: "<<weightp;
+}
