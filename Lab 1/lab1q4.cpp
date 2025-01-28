ebf2e510cbab5c5f74bbb5bf730963ba5e6e66cf
Muhammad Zain Khan <9dmuhammadzainkhan@gmail.com>
2025-01-28 09:34:36 +0500

    Update lab1q4.cpp

--- Lab 1/lab1q4.cpp
+++ Lab 1/lab1q4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#include <iomanip>
+
 int main(){
  float weightkg;
  float weightp;
@@ -7,6 +9,7 @@ int main(){
 cout <<"Enter weight in Kilograms: ";
 cin>>weightkg;
 weightp = weightkg * 2.2;
+cout<<fixed << setprecision(2);
 cout<<"\nWeight in Kiograms: "<<weightkg;
 cout<<"\nWeight in Pounds: "<<weightp;
 }
