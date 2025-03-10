aa9f661d18dc8845db652733048f981b2bc53ed2
Muhammad Zain Khan <9dmuhammadzainkhan@gmail.com>
2025-03-10 04:09:58 -0700

    Task 2.cpp

--- Lab 5/Task 2.cpp
+++ Lab 5/Task 2.cpp
@@ -1,1 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Car {
+private:
+    int id;
+    string name;
+
+public:
+    Car(int x, string n) : id(x), name(n) {}
+
+    string get_name() const {
+        return name;
+    }
+
+    int get_id() const {
+        return id;
+    }
+};
+
+class Garage {
+private:
+    vector<Car*> cars;
+
+public:
+    void parkCar(Car* e) {
+        cars.push_back(e);
+    }
+
+    void listCars() const {
+        for (Car* c : cars) {
+            cout  << c->get_id() <<". "<< c->get_name() << '\n';
+        }
+    }
+};
+
+int main() {
+    Car c1(245, "Toyota");
+    Car c2(717, "Nissan");
+    Garage g1;
+    g1.parkCar(&c1);
+    g1.parkCar(&c2);
+    g1.listCars();
+
+    return 0;
+}
 
